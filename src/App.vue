1091d76b74c45cb0580b987850d115cc9184b905
Lermy Ferrer <lermy.ferrer@insoftar.com>
2021-09-03 11:47:18 -0500

    first commit pokemon game

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <pokemonPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import pokemonPage from './pages/PokemonPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    pokemonPage
   }
 }
 </script>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>¿Quién es este Pokemon?</h1>
+  <!-- TODO: imagenes-->
+  <PokemonPicture />
+  <!-- TODO: opciones-->
+  <PokemonOptions />
+</template>
+
+<script>
+import { defineAsyncComponent, defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  name: "PokemonPage",
+  components: {
+    PokemonPicture: defineAsyncComponent(() =>
+      import("@/components/PokemonPicture.vue")
+    ),
+    PokemonOptions: defineAsyncComponent(() =>
+      import("@/components/PokemonOptions.vue")
+    ),
+  },
+});
+</script>
