393b2f0572a9846252162d81519277c087945b60
Lermy Ferrer <lermy.ferrer@insoftar.com>
2021-09-03 18:31:21 -0500

    adelanto de juego pokemon

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,13 +1,62 @@
 <template>
-  <h3>Pokemon imagen</h3>
+  <div class="pokemon-container">
+    <img
+      :src="imgSrc"
+      class="hidden-pokemon"
+      alt="pokemon"
+    />
+    <img
+      :src="imgSrc"
+      class="fade-in"
+      alt="pokemon"
+      v-if="showPokemon"
+    />
+  </div>
 </template>
 
 <script>
-export default {
+import { defineComponent, computed, ref } from "vue";
 
-}
+export default defineComponent({
+  props: {
+    pokemonId: {
+      type: Number,
+      required: true
+    },
+    showPokemon: {
+      type: Boolean,
+      required: true,
+      default: false
+    }
+  },
+  setup(props) {
+    const urlImg = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${props.pokemonId}.svg`
+    const url = ref( urlImg );
+    const imgSrc = computed(() => url.value);
+
+    return {
+      url,
+      imgSrc
+    };
+  },
+});
 </script>
 
-<style>
-
+<style scoped>
+.pokemon-container {
+  height: 200px;
+}
+img {
+  height: 200px;
+  position: absolute;
+  right: 40%;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+.hidden-pokemon {
+  filter: brightness(0);
+}
 </style>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,13 +1,20 @@
 <template>
-  <h1>¿Quién es este Pokemon?</h1>
-  <!-- TODO: imagenes-->
-  <PokemonPicture />
-  <!-- TODO: opciones-->
-  <PokemonOptions />
+  <h1 v-if="!pokemon">Espere por favor...</h1>
+  <div v-else>
+    <h1>¿Quién es este Pokemon?</h1>
+    <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon" />
+    <PokemonOptions :pokemons="pokemonArr.pokemons" @selected="checkAnswer"/>
+
+   <div v-if="showAnswer">
+      <h2 class="fade-in">{{message}}</h2>
+      <button @click="newGame">Nuevo Juego</button>
+   </div>
+  </div>
 </template>
 
 <script>
-import { defineAsyncComponent, defineComponent } from "@vue/runtime-core";
+import { defineAsyncComponent, defineComponent, onMounted, ref } from "vue";
+import getPokemonOptions from "../helpers/getPokemonOptions";
 
 export default defineComponent({
   name: "PokemonPage",
@@ -18,6 +25,54 @@ export default defineComponent({
     PokemonOptions: defineAsyncComponent(() =>
       import("@/components/PokemonOptions.vue")
     ),
+  },
+  setup() {
+    const pokemonArr = ref([]);
+    const pokemon = ref(null);
+    const showPokemon = ref(false);
+    const showAnswer = ref(false);
+    const message = ref('')
+
+    const mixPokemonArr = async () => {
+      pokemonArr.value = await getPokemonOptions();
+
+      const rndInt = Math.floor(Math.random() * 4)
+      pokemon.value = pokemonArr.value.pokemons[ rndInt ]
+    };
+
+    const checkAnswer = (selectedId) => {
+      showPokemon.value = true;
+      showAnswer.value = true;
+
+      if (selectedId === pokemon.value.id) {
+        message.value = `Es correcto, ${pokemon.value.name}`
+      } else {
+        message.value = `Oops, era, ${pokemon.value.name}`
+      }
+    }
+
+    const newGame = () => {
+      showPokemon.value = false
+      showAnswer.value = false
+      pokemonArr.value = []
+      pokemon.value = null
+      mixPokemonArr()
+    }
+
+    onMounted(() => {
+      mixPokemonArr()
+    })
+
+    return {
+      newGame,
+      message,
+      pokemon,
+      showAnswer,
+      pokemonArr,
+      mixPokemonArr,
+      showPokemon,
+      checkAnswer
+    };
   },
 });
 </script>
